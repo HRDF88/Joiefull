9711a103bf8c3de682ea0c7cfb18823ea059c4e2
HRDF88 <julien.seguin@hotmail.fr>
2025-01-28 12:26:41 +0100

    Sauvegarde du 28/01/2025, Refactor couche Data suivant principes clean architecture (mapper pour UI Model), mappage transférer dans HomeViewModel gràce à la classe ProductUIMapper + deportation des UiStates. Affichage des produits OK sur HomeFragment. JAVADOC + Réindentation OK

--- app/src/main/java/com/nedrysystems/joiefull/data/repositoryInterface/GetProductRepositoryInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.nedrysystems.joiefull.data.repositoryInterface
-
-import com.nedrysystems.joiefull.domain.model.ProductUiModel
-
-interface GetProductRepositoryInterface {
-    suspend fun getProducts(): List<ProductUiModel>
-}
